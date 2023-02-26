97ae490ad95dac0d7e149311fc67073c897464fd
Michaela Brown <m_brownbrown@yahoo.com>
2023-02-25 19:40:36 -0800

    seeds and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,16 @@ class ApplicationController < Sinatra::Base
     "Welcome to Michaela's Property Management System!".to_json
   end
 
-  
+  get "/properties" do
+    Property.all.to_json
+  end
+
+  get "/types" do
+    Type.all.to_json
+  end
+
+  get "/records" do
+    Record.all.to_json
+  end
 
 end
