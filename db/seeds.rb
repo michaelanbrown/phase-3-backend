f50c7cab7db52d983403998cc9f72509694c29e9
Michaela Brown <m_brownbrown@yahoo.com>
2023-03-06 17:21:16 -0800

    reseed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ Property.create(street_address: "2504 Las Palomas Loop", city: "Lincoln", state:
 Property.create(street_address: "1211 Zinfandel Drive", city: "Roseville", state: "CA", purchase_price: 489900, square_feet: 2189, garage_spaces: 2, link: "https://i.imgur.com/8sRjnAg.jpg", type: Type.find_by(property_type: "Single family"), flip_status: "Complete")
 Property.create(street_address: "118 Mercy Circle", city: "Hardeeville", state: "SC", purchase_price: nil, square_feet: 35800, garage_spaces: 0, link: "https://i.imgur.com/W8Z2I7H.jpg", type: Type.find_by(property_type: "Apartment Building"), flip_status: nil)
 Property.create(street_address: "35 Heritage Rd", city: "Signal Mountain", state: "TN", purchase_price: nil, square_feet: 3530, garage_spaces: 0, link: "https://i.imgur.com/3FeNq8B.jpg", type: Type.find_by(property_type: "Multifamily"), flip_status: nil)
-Property.create(street_address: "4396 Amnicola Hwy", city: "Chattanooga", state: "TN", purchase_price: nil, square_feet: 5464, garage_spaces: nil, link: "https://i.imgur.com/ZmGMcpI.jpg", type: Type.find_by(property_type: "Commercial/Business"), flip_status: nil)
+Property.create(street_address: "4396 Amnicola Hwy", city: "Chattanooga", state: "TN", purchase_price: nil, square_feet: 5464, garage_spaces: 0, link: "https://i.imgur.com/ZmGMcpI.jpg", type: Type.find_by(property_type: "Commercial/Business"), flip_status: nil)
 
 Record.create(mortgage_payment: 1735, hoa_payment: 179, property_management_payment: 100, gross_income: 1900, property: Property.find_by(street_address: "2504 Las Palomas Loop"))
 Record.create(mortgage_payment: 1747, hoa_payment: 185, property_management_payment: 100, gross_income: 2200, property: Property.find_by(street_address: "2504 Las Palomas Loop"))
